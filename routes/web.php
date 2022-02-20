3a680ccf33c0b5197084560067d2dc1af4463846
avxman <downloadsite.x@gmail.com>
2022-02-20 13:22:07 +0200

    Тестовый запрос 6

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome')->with(['result'=>'Главная страница v1.0.5']);
+    return view('welcome')->with(['result'=>'Главная страница v1.0.6']);
 });
 
 Route::get('/jobs', function () {
