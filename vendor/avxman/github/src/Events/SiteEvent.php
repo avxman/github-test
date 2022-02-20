71a7724020879428cabbdc0a560cea9a2bd44ed9
avxman <downloadsite.x@gmail.com>
2022-02-20 13:25:51 +0200

    Изменение в коде 1.0.2

--- vendor/avxman/github/src/Events/SiteEvent.php
+++ vendor/avxman/github/src/Events/SiteEvent.php
@@ -40,6 +40,7 @@ class SiteEvent extends BaseEvent
             $comm = $this->commandGenerate("stash save --keep-index");
             if(Str::contains(Str::lower($comm), 'saved')){
                 $command = $this->commandGenerate("pull");
+                $command .= PHP_EOL.'В процессе обновлении найден конфликт, а именно, на сайте вручную внесли изменения: '.PHP_EOL.$comm;
             }
             else{
                 $command = $comm;
